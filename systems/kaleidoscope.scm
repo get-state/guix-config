3a12fa9013d3fb91836537b125fd5fa0253ee5fb
Mazin Al Haddad <mazin@getstate.dev>
2024-12-29 11:16:25 +0300

    NVIM CHANGES, KALEIDOSCOPE SUPER-IO, LOWER FONT SIZE ALACRITTY
    * Adds super-io service, to load nct6775.
    * luafmt on nvim config.
    * Switch to kanagawa in nvim.
    * Change font size to 12 on alacritty.

--- systems/kaleidoscope.scm
+++ systems/kaleidoscope.scm
@@ -1,8 +1,12 @@
 (define-module (systems kaleidoscope)
   #:use-module (systems base-system)
+  #:use-module (gnu services linux)
   #:use-module (gnu)
   #:use-module (guix))
 
 (operating-system
   (inherit base-system)
+  (services
+   (append (list (simple-service 'super-io kernel-module-loader-service-type
+                                 '("nct6775"))) default-services))
   (host-name "kaleidoscope"))
