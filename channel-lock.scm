7baa89e09323fc9b29f0b67dbed57e513b0fbcb4
Mazin Al Haddad <mazin@getstate.dev>
2025-01-09 11:28:24 +0300

    UPDATE CHANNELLOCK

--- channel-lock.scm
+++ channel-lock.scm
@@ -3,7 +3,7 @@
         (url "https://gitlab.com/nonguix/nonguix")
         (branch "master")
         (commit
-          "10e3c2bcaedaba121eec0e255d366a080082cf0a")
+          "3195ddf9f125fd8f9c80afc5d0868106bd90a20d")
         (introduction
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
@@ -25,7 +25,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "1a62f687e504c67f26693f3bc60a90e503ab4ea9")
+          "7f27dc47c52886b785359799b6dc67b61f638544")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
