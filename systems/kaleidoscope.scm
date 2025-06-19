3d9f75caaf0940516b74100c8cefea66e76f3b8b
Mazin Al Haddad <mazin@getstate.dev>
2025-06-19 23:30:58 +0300

    ADD NVIDIA BINARY DRIVER
    * Do not set it as main display.

--- systems/kaleidoscope.scm
+++ systems/kaleidoscope.scm
@@ -1,12 +1,19 @@
 (define-module (systems kaleidoscope)
   #:use-module (systems base-system)
   #:use-module (gnu services linux)
+  #:use-module (nongnu services nvidia)
   #:use-module (gnu)
   #:use-module (guix))
 
 (operating-system
   (inherit base-system)
   (services
-   (append (list (simple-service 'super-io kernel-module-loader-service-type
+   (append (list (service nvidia-service-type)
+                 (simple-service 'super-io kernel-module-loader-service-type
                                  '("nct6775"))) default-services))
+  (kernel-arguments (append '("modprobe.blacklist=nouveau"
+                              ;; Set this if the card is not used for displaying or
+                              ;; you're using Wayland:
+                              "nvidia_drm.modeset=0")
+                            %default-kernel-arguments))
   (host-name "kaleidoscope"))
