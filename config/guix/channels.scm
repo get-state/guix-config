745673794da1c1e6ba45d23388ba8109d01d0d8b
Mazin Al Haddad <mazin@getstate.dev>
2025-08-03 21:30:50 +0300

    ADJUST CHANNELS

--- config/guix/channels.scm
+++ config/guix/channels.scm
@@ -7,23 +7,15 @@
            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
            (openpgp-fingerprint
             "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
-       (channel
-         (name 'tailscale)
-         (url "https://github.com/umanwizard/guix-tailscale")
-         (branch "main")
-         (introduction
+        (channel
+        (name 'abbe)
+        (url "https://codeberg.org/group/guix-modules.git")
+        (branch "mainline")
+        (introduction
           (make-channel-introduction
-           "c72e15e84c4a9d199303aa40a81a95939db0cfee"
-           (openpgp-fingerprint "9E53FC33B8328C745E7B31F70226C10D7877B741"))))
-       (channel
-         (name 'saayix)
-         (branch "main")
-         (url "https://codeberg.org/look/saayix")
-         (introduction
-          (make-channel-introduction
-           "12540f593092e9a177eb8a974a57bb4892327752"
-           (openpgp-fingerprint
-            "3FFA 7335 973E 0A49 47FC  0A8C 38D5 96BE 07D3 34AB"))))
+            "8c754e3a4b49af7459a8c99de130fa880e5ca86a"
+            (openpgp-fingerprint
+              "F682 CDCC 39DC 0FEA E116  20B6 C746 CFA9 E74F A4B0"))))
        (channel
          (name 'small-guix)
          (url "https://codeberg.org/fishinthecalculator/small-guix.git")
@@ -34,4 +26,7 @@
            "f260da13666cd41ae3202270784e61e062a3999c"
            (openpgp-fingerprint
             "8D10 60B9 6BB8 292E 829B  7249 AED4 1CC1 93B7 01E2"))))
+        (channel
+        (name 'guix-hpc-non-free)
+        (url "https://gitlab.inria.fr/guix-hpc/guix-hpc-non-free.git"))
        %default-channels)
