99e52edcf66c80bd0ca041c481acef96087ff93d
Mazin Al Haddad <mazin@getstate.dev>
2025-12-19 18:49:46 +0000

    Remove extra modules

--- systems/avalon.scm
+++ systems/avalon.scm
@@ -1,7 +1,5 @@
 (define-module (systems kaleidoscope)
   #:use-module (systems base-system)
-  #:use-module (gnu services linux)
-  #:use-module (nongnu services nvidia)
   #:use-module (gnu))
 
 (operating-system
