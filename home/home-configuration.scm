0b6084528bd31c6229b40cc76063d362459548ec
Mazin Al Haddad <mazin@getstate.dev>
2026-01-31 20:58:32 +0000

    ADD ueberzugpp

--- home/home-configuration.scm
+++ home/home-configuration.scm
@@ -32,6 +32,7 @@
                                             "adwaita-icon-theme"
                                             "hicolor-icon-theme"
                                             "ppsspp"
+                                            "ueberzugpp"
                                             "qogir-icon-theme"
                                             "gnome-themes-extra"
                                             "github-cli"
@@ -92,141 +93,143 @@
                                             "aerc"
                                             "mpv")))
 
-  ;; Below is the list of Home services.  To search for available
-  ;; services, run 'guix home search KEYWORD' in a terminal.
-  (services
-   (append (list (simple-service 'xdg-user-directories-config-service
-                                 home-xdg-user-directories-service-type
-                                 (home-xdg-user-directories-configuration (desktop
-                                                                           "$HOME/Desktop")
-                                                                          (documents
-                                                                           "$HOME/Documents")
-                                                                          (download
-                                                                           "$HOME/Downloads")
-                                                                          (music
-                                                                           "$HOME/Music")
-                                                                          (pictures
-                                                                           "$HOME/Pictures")
-                                                                          (publicshare
-                                                                           "$HOME/Public")
-                                                                          (templates
-                                                                           "$HOME/Templates")
-                                                                          (videos
-                                                                           "$HOME/Videos")))
+;; Below is the list of Home services.  To search for available
+;; services, run 'guix home search KEYWORD' in a terminal.
+(services (append (list (simple-service 'xdg-user-directories-config-service
+                         home-xdg-user-directories-service-type
+                         (home-xdg-user-directories-configuration (desktop
+                                                                   "$HOME/Desktop")
+                                                                  (documents
+                                                                   "$HOME/Documents")
+                                                                  (download
+                                                                   "$HOME/Downloads")
+                                                                  (music
+                                                                   "$HOME/Music")
+                                                                  (pictures
+                                                                   "$HOME/Pictures")
+                                                                  (publicshare
+                                                                   "$HOME/Public")
+                                                                  (templates
+                                                                   "$HOME/Templates")
+                                                                  (videos
+                                                                   "$HOME/Videos")))
 
-                 ;; Dbus is needed as a dependency for pipewire
-                 (service home-dbus-service-type)
-                 ;; Configure pipewire service, should enable pulseaudio
-                 (service home-pipewire-service-type)
+                        ;; Dbus is needed as a dependency for pipewire
+                        (service home-dbus-service-type)
+                        ;; Configure pipewire service, should enable pulseaudio
+                        (service home-pipewire-service-type)
 
-                 (service home-fish-service-type
-                          (home-fish-configuration (config (list (local-file
-                                                                  "../config/fish/config.fish")))))
+                        (service home-fish-service-type
+                                 (home-fish-configuration (config (list (local-file
+                                                                         "../config/fish/config.fish")))))
 
-                 (simple-service 'some-useful-env-vars-service
-                                 home-environment-variables-service-type
-                                 `(("EDITOR" . "nvim")
-                                   ("QT_QPA_PLATFORMTHEME" . "gtk3")
-                                   ("GTK_THEME" . "Adwaita:dark")
-                                   ("TERMINAL" . "alacritty -e")))
+                        (simple-service 'some-useful-env-vars-service
+                         home-environment-variables-service-type
+                         `(("EDITOR" . "nvim")
+                           ("QT_QPA_PLATFORMTHEME" . "gtk3")
+                           ("GTK_THEME" . "Adwaita:dark")
+                           ("TERMINAL" . "alacritty -e")))
 
-                 (service home-gpg-agent-service-type
-                          (home-gpg-agent-configuration (pinentry-program (file-append
-                                                                           pinentry-tty
-                                                                           "/bin/pinentry-curses"))
-                                                        (ssh-support? #f)))
+                        (service home-gpg-agent-service-type
+                                 (home-gpg-agent-configuration (pinentry-program
+                                                                (file-append
+                                                                 pinentry-tty
+                                                                 "/bin/pinentry-curses"))
+                                                               (ssh-support?
+                                                                             #f)))
 
-                 (service home-shepherd-service-type
-                          (home-shepherd-configuration (services (list (shepherd-service
-                                                                        (provision '
-                                                                         (mpd))
-                                                                        (start #~
-                                                                         (make-system-constructor
-                                                                          "mpd")))))))
+                        (service home-shepherd-service-type
+                                 (home-shepherd-configuration (services (list (shepherd-service
+                                                                               (provision '
+                                                                                (mpd))
+                                                                               
+                                                                               (start #~
+                                                                                (make-system-constructor
+                                                                                 "mpd")))))))
 
-                 (service home-openssh-service-type
-                          (home-openssh-configuration (add-keys-to-agent "yes")))
+                        (service home-openssh-service-type
+                                 (home-openssh-configuration (add-keys-to-agent
+                                                              "yes")))
 
-                 (service home-ssh-agent-service-type
-                          (home-ssh-agent-configuration (extra-options '("-t"
-                                                                         "1h30m"))))
+                        (service home-ssh-agent-service-type
+                                 (home-ssh-agent-configuration (extra-options '
+                                                                              ("-t"
+                                                                               "1h30m"))))
 
-                 (service home-x11-service-type)
+                        (service home-x11-service-type)
 
-                 (simple-service 'carapace-nushell-setup
-                                 home-activation-service-type
-                                 #~(begin
-                                     (let ((cache-dir (string-append (getenv
-                                                                      "HOME")
-                                                       "/.cache/nushell")))
-                                       (mkdir-p cache-dir)
-                                       (system (string-append #$carapace
-                                                "/bin/carapace _carapace nushell > "
-                                                cache-dir "/carapace.nu")))))
+                        (simple-service 'carapace-nushell-setup
+                                        home-activation-service-type
+                                        #~(begin
+                                            (let ((cache-dir (string-append (getenv
+                                                                             "HOME")
+                                                              "/.cache/nushell")))
+                                              (mkdir-p cache-dir)
+                                              (system (string-append #$carapace
+                                                       "/bin/carapace _carapace nushell > "
+                                                       cache-dir
+                                                       "/carapace.nu")))))
 
-                 (service home-unclutter-service-type
-                          (home-unclutter-configuration (unclutter
-                                                         unclutter-xfixes)
-                                                        (idle-timeout 8)))
+                        (service home-unclutter-service-type
+                                 (home-unclutter-configuration (unclutter
+                                                                unclutter-xfixes)
+                                                               (idle-timeout 8)))
 
-                 ;; Moves fonts and other configs in $HOME.
-                 (simple-service `home-config home-files-service-type
-                                 `((".xinitrc" ,(local-file
-                                                 "../config/other/xinitrc"))
-                                   (".Xresources" ,(local-file
-                                                    "../config/other/Xresources"))
-                                   (".gtkrc-2.0" ,(local-file
-                                                   "../config/other/gtkrc-2.0"))
-                                   (".local/share/fonts" ,(local-file
-                                                           "../config/fonts"
-                                                           #:recursive? #t))))
+                        ;; Moves fonts and other configs in $HOME.
+                        (simple-service `home-config home-files-service-type
+                                        `((".xinitrc" ,(local-file
+                                                        "../config/other/xinitrc"))
+                                          (".Xresources" ,(local-file
+                                                           "../config/other/Xresources"))
+                                          (".gtkrc-2.0" ,(local-file
+                                                          "../config/other/gtkrc-2.0"))
+                                          (".local/share/fonts" ,(local-file
+                                                                  "../config/fonts"
+                                                                  #:recursive?
+                                                                  #t))))
 
-                 ;; Handles configs that adhere to XDG.
-                 (simple-service `wm-config
-                                 home-xdg-configuration-files-service-type
-                                 `(("i3/config" ,(local-file
-                                                  "../config/i3/config-carbon"))
-                                   ("sway/config" ,(local-file
-                                                    "../config/sway/config"))
-                                   ("alacritty.toml" ,(local-file
-                                                       "../config/alacritty/alacritty-carbon.toml"))
+                        ;; Handles configs that adhere to XDG.
+                        (simple-service `wm-config
+                         home-xdg-configuration-files-service-type
+                         `(("i3/config" ,(local-file
+                                          "../config/i3/config-carbon"))
+                           ("sway/config" ,(local-file "../config/sway/config"))
+                           ("alacritty.toml" ,(local-file
+                                               "../config/alacritty/alacritty-carbon.toml"))
 
-                                   ("nushell/login.nu" ,(local-file
-                                                         "../config/nu/login.nu"))
+                           ("nushell/login.nu" ,(local-file
+                                                 "../config/nu/login.nu"))
 
-                                   ("fontconfig/conf.d/99-fonts.conf" ,(local-file
-                                                                        "../config/fontconfig/fonts.conf"))
-                                   ("picom/picom.conf" ,(local-file
-                                                         "../config/picom/picom.conf"))
+                           ("fontconfig/conf.d/99-fonts.conf" ,(local-file
+                                                                "../config/fontconfig/fonts.conf"))
+                           ("picom/picom.conf" ,(local-file
+                                                 "../config/picom/picom.conf"))
 
-                                   ("nix/nix.conf" ,(local-file
-                                                     "../config/nix/nix.conf"))
+                           ("nix/nix.conf" ,(local-file
+                                             "../config/nix/nix.conf"))
 
-                                   ("gtk-3.0/settings.ini" ,(local-file
-                                                             "../config/other/gtk3-settings.ini"))
+                           ("gtk-3.0/settings.ini" ,(local-file
+                                                     "../config/other/gtk3-settings.ini"))
 
-                                   ("mimeapps.list" ,(local-file
-                                                      "../config/mime/mimeapps.list"))
+                           ("mimeapps.list" ,(local-file
+                                              "../config/mime/mimeapps.list"))
 
-                                   ("pipewire/pipewire.conf.d" ,(local-file
-                                                                 "../config/pipewire"
-                                                                 #:recursive?
-                                                                 #t))
+                           ("pipewire/pipewire.conf.d" ,(local-file
+                                                         "../config/pipewire"
+                                                         #:recursive? #t))
 
-                                   ("polybar/shades" ,(local-file
-                                                       "../config/shades"
-                                                       #:recursive? #t))
+                           ("polybar/shades" ,(local-file "../config/shades"
+                                                          #:recursive? #t))
 
-                                   ("newsboat" ,(local-file
-                                                 "../config/newsboat"
-                                                 #:recursive? #t))
+                           ("newsboat" ,(local-file "../config/newsboat"
+                                                    #:recursive? #t))
 
-                                   ("lf" ,(local-file "../config/lf"
-                                                      #:recursive? #t))
+                           ("lf" ,(local-file "../config/lf"
+                                              #:recursive? #t))
 
-                                   ;; ("aerc" ,(local-file "../config/aerc"
-                                   ;; #:recursive? #t))
-                                   
-                                   ("ncmpcpp" ,(local-file "../config/ncmpcpp"
-                                                #:recursive? #t)))))
-           %base-home-services)))
+                           ;; ("aerc" ,(local-file "../config/aerc"
+                           ;; #:recursive? #t))
+                           
+                           ("ncmpcpp" ,(local-file "../config/ncmpcpp"
+                                                   #:recursive? #t)))))
+                  %base-home-services)))
